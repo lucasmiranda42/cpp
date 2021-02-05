7f00e32b49edce5fe582c9ed6734bfe90d73b2c6
lucas_miranda <lucasmiranda42@gmail.com>
2021-02-05 18:01:00 +0100

    Exercise 1

--- w0I5mayhEem-LxI3AmSYig_d4fa7d243d7e4186895a4cb2befed1e6_hello-world-given-files/main.cpp
+++ w0I5mayhEem-LxI3AmSYig_d4fa7d243d7e4186895a4cb2befed1e6_hello-world-given-files/main.cpp
@@ -8,10 +8,15 @@
 **/
 
 // You need to include some header(s) here!
-
+#include <iostream>
 
 // You need to define your main() function here!
+int main() {
 
+    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Greetings from Illinois!" << std::endl;
+
+}
 
 // Notes:
 
